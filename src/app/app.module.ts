d39e2bbe25434dcab475d954357abaec550197e0
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-13 16:44:22 +0200

    Starting files for a new mini-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TypingCardComponent } from './typing-card/typing-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypingCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/typing-card/typing-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-card',
+  templateUrl: './typing-card.component.html',
+  styleUrls: ['./typing-card.component.css']
+})
+export class TypingCardComponent {
+
+}
