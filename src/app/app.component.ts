767279e7c696066768acdbea8e13e239de6e2714
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-14 11:21:59 +0200

    Practicing basics from lectures before

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing';
+  randomText = lorem.sentence();
+  message = '';
+
+  OnInput = (value: Event) => {
+    const target = value.target as HTMLInputElement;
+    this.message = target.value;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TypingCardComponent } from './typing-card/typing-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TypingCardComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
